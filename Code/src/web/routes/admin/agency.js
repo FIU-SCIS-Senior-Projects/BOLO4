f4692f3c6243b68aa5442a339a9401ce08ef11ae
pmess002 <pgio24@hotmail.com>
2016-02-16 21:38:16 -0500

    BOLO Details and Agency Management update

--- Code/src/web/routes/admin/agency.js
+++ Code/src/web/routes/admin/agency.js
@@ -28,20 +28,50 @@ function isImage ( fileDTO ) {
 function getAgencyAttachments ( fields ) {
     var result = [];
     var fileDTO;
+    var validateShield = false;
+    var validateLogo = false;
 
     if ( fields.logo_upload && isImage( fields.logo_upload  ) ) {
         fileDTO = _.assign( {}, fields.logo_upload );
         fileDTO.name = 'logo';
         result.push( fileDTO );
+        validateLogo = true;
     }
 
     if ( fields.shield_upload && isImage( fields.shield_upload ) ) {
         fileDTO = _.assign( {}, fields.shield_upload );
         fileDTO.name = 'shield';
         result.push( fileDTO );
+        validateShield = true;
     }
 
     return ( result.length ) ? result : null;
+}
+
+/**
+ * Validating whther or not the fields in the form have been left empty.
+ * If one of the fields has been left empty, validateFields will return false.
+ */
+function validateFields (fields){
+  var fieldValidator = true;
+
+  if(fields.name == ""){
+    fieldValidator = false;
+  }
+  if(fields.address == ""){
+    fieldValidator = false;
+  }
+  if(fields.city == ""){
+    fieldValidator = false;
+  }
+  if(fields.zip == ""){
+    fieldValidator = false;
+  }
+  if(fields.phone == ""){
+    fieldValidator = false;
+  }
+
+  return fieldValidator;
 }
 
 
@@ -72,6 +102,18 @@ module.exports.getCreateForm = function (req, res) {
 module.exports.postCreateForm = function ( req, res, next ) {
     parseFormData( req ).then( function ( formDTO ) {
         var agencyDTO = agencyService.formatDTO( formDTO.fields );
+        var atts = getAgencyAttachments( formDTO.fields );
+        var formFields = validateFields(formDTO.fields);
+        if(formFields == false){
+          req.flash(GFERR, 'No field can be left empty. This information is required');
+          res.redirect('back');
+          throw new FormError();
+        }
+        if(atts == null){
+          req.flash(GFERR, 'Images not uploaded. Logo and Shield are required');
+          res.redirect('back');
+          throw new FormError();
+        }
         var atts = getAgencyAttachments( formDTO.fields );
         var result = agencyService.createAgency( agencyDTO, atts );
         return Promise.all( [ result, formDTO ] );
