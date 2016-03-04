d90d8ed98414bbfaea104aef9caffb6db8ef1a92
ahena001 <ahena001@fiu.edu>
2016-03-03 21:31:37 -0500

    cleanded code for pull request

--- Code/src/web/routes/bolos.js
+++ Code/src/web/routes/bolos.js
@@ -579,32 +579,24 @@ router.get( '/bolo/details/:id', function ( req, res, next ) {
     }).then( function ( agency ) {
         console.log(agency);
         data.agency = agency;
-<<<<<<< HEAD
+
         generatePDF(data);
-=======
-       // generatePDF(data.bolo.data);
->>>>>>> 9f5ce9041db27b92ac2cb87a44c3c9228e17e746
+
         res.render( 'bolo-details', data );
     }).catch( function ( error ) {
         next( error );
     });
-
-
 });
 
+
+/**
+ * Generates PDF from bolo / agency information
+ */
 function generatePDF(data){
   var doc = new PDFDocument();
   var someData = {};
-  doc.pipe(fs.createWriteStream('src/web/public/pdf/' + data.bolo.id + ".pdf"));  //creating a write stream
-        //to write the content on the file system
-
-  //var x, y = 100;
-  //var keys = Object.keys(data.bolo.attachments);
-  //var testImage = fs.readFileSync('src/web/public/img/nopic.png');
-
-  // gets image from boloRepository and chains it to embed on (PDFDocument)doc
+  doc.pipe(fs.createWriteStream('src/web/public/pdf/' + data.bolo.id + ".pdf"));
   doc.fontSize(8);
-
   doc.fillColor('red');
   doc.text("UNCLASSIFIED// FOR OFFICIAL USE ONLY// LAW ENFORCEMENT SENSITIVE", 120,15)
     .moveDown(0.25);
@@ -628,43 +620,33 @@ function generatePDF(data){
   doc.font('Times-Roman')
     .text("Name: "  + data.bolo['firstName'] + " " + data.bolo['lastName'], 350)
     .moveDown();
-
   doc.font('Times-Roman')
      .text("Race: "  + data.bolo['race'], 350)
      .moveDown();
-
   doc.font('Times-Roman')
      .text("DOB: "  + data.bolo['dob'], 350)
      .moveDown();
-
   doc.font('Times-Roman')
     .text("License#: "  + data.bolo['dlNumber'], 350)
     .moveDown();
-
    doc.font('Times-Roman')
       .text("Height: "  + data.bolo['height'], 350)
       .moveDown();
-
    doc.font('Times-Roman')
       .text("Weight: "  + data.bolo['weight'] + "lbs", 350)
       .moveDown();
-
-
    doc.font('Times-Roman')
       .text("Address: "  + data.bolo['address'], 350)
       .moveDown();
-
    doc.font('Times-Roman')
       .text("Sex: "  + data.bolo['sex'], 350)
       .moveDown();
-
    doc.font('Times-Roman')
       .text("Hair Color: "  + data.bolo['hairColor'], 350)
       .moveDown();
    doc.font('Times-Roman')
       .text("Tattoos/Scars: "  + data.bolo['tattoos'], 350)
       .moveDown();
-
    doc.font('Times-Roman')
       .text("Additional: ",  15,465)
       .moveDown(0.25);
@@ -677,69 +659,25 @@ function generatePDF(data){
    doc.font('Times-Roman')
       .text(data.bolo['summary'], {width : 200})
       .moveDown(5);
-
-      doc.font('Times-Roman')
-         .text("Any Agency having questions regarding this document may contact: "
-         + data.bolo.authorFName
-         + " "
-         + data.bolo.authorLName,  15);
-
-    //  doc.end();
-
-
-  //boloService.getAttachment(data.bolo.id, 'featured').then(function (attDTO)
-  //agencyService.getAttachment(data.agency.id, 'logo').then(function (logoDTO)
-  boloService.getAttachment(data.bolo.id, 'featured').then(function (attDTO){
-          someData.featured = attDTO.data;
-          doc.image(someData.featured, 15, 150, {width: 300, height:300});
-        //  doc.image(someData.logo, 15, 15, {height: 100});
-        //  doc.end();
-        //  return attDTO.data
-        return agencyService.getAttachment(data.agency.data.id, 'logo')
-  }).then( function(logoDTO){
-          someData.logo = logoDTO.data;
-          doc.image(someData.logo, 15, 15, {height: 100});
-          return agencyService.getAttachment(data.agency.data.id, 'shield')
-  }).then(function(shieldDTO){
-          someData.shield = shieldDTO.data;
-          doc.image(someData.shield, 500, 15, {height: 100});
-          doc.end();
-  });
-  // .then( function(featuredDTO){
-  //     someData.featured = featuredDTO.data;
-  //     return agencyService.getAttachment(data.agency.id, 'shield')
-  // }).then(function(shieldDTO){
-  //   someData.shield = shieldDTO.data;
-  //   doc.image(someData.logo, 15, 15, {height: 100});
-  //   doc.image(someData.featured, 15, 150, {width: 300, height:300});
-  //   doc.image(someData.shield, 500, 15, {height: 100});
-  //   doc.end();
-  //   return agencyService.getAttachment(data.agency.id, 'shield')
-  // });
-
-
-  // });
-
-
-
-
-<<<<<<< HEAD
-=======
-router.get( '/bolo/viewPDF/:id', function ( req, res, next ) {
-   var data = {};
-  // instead of running the services again...an object should kept in session
-   boloService.getBolo( req.params.id )
-   .then( function ( bolo ) {
-       data.bolo = bolo;
-       return agencyService.getAgency( bolo.agency );
-   }).then( function ( agency ) {
-       data.agency = agency;
-      //  res.render( 'bolo-pdf', data );
->>>>>>> 9f5ce9041db27b92ac2cb87a44c3c9228e17e746
-
-
+   doc.font('Times-Roman')
+      .text("Any Agency having questions regarding this document may contact: "
+      + data.bolo.authorFName
+      + " "
+      + data.bolo.authorLName,  15);
+   boloService.getAttachment(data.bolo.id, 'featured').then(function (attDTO){
+      someData.featured = attDTO.data;
+      doc.image(someData.featured, 15, 150, {width: 300, height:300});
+      return agencyService.getAttachment(data.agency.data.id, 'logo')
+   }).then( function(logoDTO){
+      someData.logo = logoDTO.data;
+      doc.image(someData.logo, 15, 15, {height: 100});
+      return agencyService.getAttachment(data.agency.data.id, 'shield')
+   }).then(function(shieldDTO){
+      someData.shield = shieldDTO.data;
+      doc.image(someData.shield, 500, 15, {height: 100});
+      doc.end();
+   });
 }
-
 
 // handle requests for bolo attachments
 function getAttachment ( req, res ) {
@@ -749,11 +687,9 @@ function getAttachment ( req, res ) {
           //  console.log(attDTO.data + " A:OIJF:OIAEJRO:IJE:ROIJW:EOIRJWE:OIR");
             res.send(attDTO.data);
         });
-
 }
 
 
     router.get('/bolo/asset/:boloid/:attname', getAttachment);
     router.getAttachment = getAttachment;
-
     module.exports = router;
